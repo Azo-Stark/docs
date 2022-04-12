4c52d73d809917690e831207af6518c76a5733dd
liuzhou <liuzhou@shineyue.com>
2022-04-12 20:44:34 +0800

    更新时间

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,6 +3,7 @@ module.exports = {
     description: 'Just playing around',
     
     themeConfig: {
+        LastUpdated: '跟新时间',
         logo: '/assets/img/logo.png',
         head: [
             ['link', { rel: 'icon', href: '/assets/img/logo.png' }]
@@ -20,6 +21,18 @@ module.exports = {
             },
             { text: 'GitHub', link: 'https://github.com/Azo-Stark' },
         ],
-        sidebar: 'auto'
+        sidebar: [
+            {
+                title: '关于',   // 必要的
+                path: '/guide/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+                collapsable: false, // 可选的, 默认值是 true,
+                sidebarDepth: 1,    // 可选的, 默认值是 1
+                children: [
+                    '/guide/one',
+                    '/guide/two',
+                    '/guide/guides/three'
+                ]
+            }
+        ]
     }
 }
